3809ce592e09d8e08b3638888df8e542f35f0c41
LZ <liuzhellll@hotmail.com>
2019-10-31 09:46:31 +0800

    添加日志

---
+++ Metro.Logging.File.UI/ConfigHelper.cs
@@ -0,0 +1,32 @@
+﻿using Metro.Logging.File.UI.Models;
+using System;
+using System.IO;
+
+namespace Metro.Logging.File.UI
+{
+    public static class ConfigHelper
+    {
+        private static readonly string _dir = Path.Combine(AppContext.BaseDirectory, "logs");
+        private static readonly string _path;
+
+        static ConfigHelper()
+        {
+            if (!Directory.Exists(_dir))
+            {
+                Directory.CreateDirectory(_dir);
+            }
+            _path = Path.Combine(_dir, "config.json");
+        }
+
+        public static ConfigModel Get()
+        {
+            if (!System.IO.File.Exists(_path)) return null;
+            return System.Text.Json.JsonSerializer.Deserialize<ConfigModel>(System.IO.File.ReadAllText(_path));
+        }
+
+        public static void Save(ConfigModel config)
+        {
+            System.IO.File.WriteAllText(_path, System.Text.Json.JsonSerializer.Serialize(config));
+        }
+    }
+}
